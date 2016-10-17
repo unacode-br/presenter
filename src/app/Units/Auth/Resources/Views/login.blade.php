56361b158b9b84546c47a1c5bea2d25109be775d
Taahsene <tay.sene@gmail.com>
2016-10-17 09:12:25 -0200

    Login mobile

--- src/app/Units/Auth/Resources/Views/login.blade.php
+++ src/app/Units/Auth/Resources/Views/login.blade.php
@@ -3,14 +3,16 @@
 @section('content')
   <div class="container" id="main">
     <div class="row vdivide">
-      <div class="col-sm-6 text-center hidden-xs">
+      <div class="col-sm-6 text-center">
         <h2><strong>Stack</strong><small>Hub</small></h2>
-        <p>
+        <p class="hidden-xs">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
       <div class="col-sm-6 text-center ">
-        <h1>Login</h1>
+        <h1 class="hidden-xs">Login</h1>
+        <h3 class="hidden-md">Login</h3>
+      </br>
         <div class="col-md-6 col-md-offset-3">
           <a href="{{ url('/auth/github') }}" class="btn btn-block btn-social btn-github">
             <span class="fa fa-github"></span> Login com Github
