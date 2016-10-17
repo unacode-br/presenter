fd2a1ca62b81a6bcbe2cbd79c8eef34d3392316f
Taahsene <tay.sene@gmail.com>
2016-10-17 08:54:39 -0200

    Ajuste login mobile

--- src/app/Units/Auth/Resources/Views/login.blade.php
+++ src/app/Units/Auth/Resources/Views/login.blade.php
@@ -3,13 +3,13 @@
 @section('content')
   <div class="container" id="main">
     <div class="row vdivide">
-      <div class="col-sm-6 text-center">
+      <div class="col-sm-6 text-center hidden-xs">
         <h2><strong>Stack</strong><small>Hub</small></h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
-      <div class="col-sm-6 text-center">
+      <div class="col-sm-6 text-center ">
         <h1>Login</h1>
         <div class="col-md-6 col-md-offset-3">
           <a href="{{ url('/auth/github') }}" class="btn btn-block btn-social btn-github">
