df78c576e1380afbb0c24d806272f84749e83811
Gustavo Marttos <marttosc@gmail.com>
2016-10-18 22:26:17 -0200

    Base view for the Top 10 Forked repositories on Github.

--- src/app/Units/Home/Resources/Views/Graphics/Trends/forks.blade.php
+++ src/app/Units/Home/Resources/Views/Graphics/Trends/forks.blade.php
@@ -1,22 +1,21 @@
 @extends('core::template.layout.app')
 
 @section('content')
-<div class="container">
-  <div class="row">
-    <h3 class="text-center">Github Trends by Forks</h3><hr>
-  </div>
-  <div class="row">
-    <div class="col-sm-4 col-md-4 sidebar">
-      <ul class="nav nav-sidebar">
-        <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-        <li>Reports</li>
-        <li>Analytics</li>
-        <li>Export</li>
-      </ul>
+    <div class="row">
+        <h3 class="text-center">Github Trends by Forks</h3>
+        <hr>
     </div>
-    <div class="col-sm-8 col-md-8 panel">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <div class="row">
+        <div class="col-sm-8 col-md-8 panel">
+            <div id="forks"></div>
+        </div>
     </div>
-  </div>
-</div>
+@endsection
+
+@section('scripts')
+    <script type="text/javascript">
+        $(function() {
+            Highcharts.chart('forks', {});
+        });
+    </script>
 @endsection
