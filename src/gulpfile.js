000d36950d89ab8257e34ecabce950c4ea0ce270
Gustavo Marttos <marttosc@gmail.com>
2016-10-18 22:13:57 -0200

    Highcharts scripts and the Blade view must be together.

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -15,8 +15,5 @@ require('laravel-elixir-vue');
 
 elixir(mix => {
     mix.sass('app.scss')
-        .webpack('app.js')
-        .scripts([
-            'highcharts/forks.js'
-        ], 'public/js/highcharts/charts.js');
+        .webpack('app.js');
 });
