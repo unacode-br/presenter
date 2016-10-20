4bc077b0839dad8fe5c99dc49eda61c55155ab8b
Gustavo Marttos <marttosc@gmail.com>
2016-10-20 19:37:39 -0200

    Use PHP date function instead of Javascript.

--- src/app/Units/Core/Resources/Views/template/snippets/footer.blade.php
+++ src/app/Units/Core/Resources/Views/template/snippets/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container-fluid">
         <div class="copyright pull-right">
-            &copy; <script>document.write(new Date().getFullYear())</script>, Feito com <i class="fa fa-heart heart"></i> by Unacode
+            &copy; {{ date('Y') }}, Feito com <i class="fa fa-heart heart"></i> by Unacode
         </div>
     </div>
 </footer>
