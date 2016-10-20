899fea66743710ff5cf3957585d00fdb044bd72e
Gustavo Marttos <marttosc@gmail.com>
2016-10-20 21:02:22 -0200

    Sidebar updated.

--- src/app/Units/Core/Resources/Views/template/snippets/sidebar.blade.php
+++ src/app/Units/Core/Resources/Views/template/snippets/sidebar.blade.php
@@ -1,30 +1,31 @@
 <!-- Sidebar -->
 <div class="sidebar" data-active-color="gray" data-active-color="purple">
-  <div class="sidebar-wrapper">
-    <div class="logo">
-      <a href="{{ url('/') }}" class="simple-text">
-        <strong>Stack</strong><small>hab</small>
-      </a>
+    <div class="sidebar-wrapper">
+        <div class="logo">
+            <a href="{{ url('/') }}" class="simple-text">
+                <strong>Stack</strong>
+                <small>hub</small>
+            </a>
+        </div>
+        <ul class="nav">
+            <li>
+                <a href="{{ url('/home') }}">
+                    <i class="fa fa-dashboard"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{ url('/graphics/stars') }}">
+                    <i class="fa fa-star"></i>
+                    <p>Top 10 Stared</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{ url('/graphics/forks') }}">
+                    <i class="fa fa-code-fork"></i>
+                    <p>Top 10 Forked</p>
+                </a>
+            </li>
+        </ul>
     </div>
-    <ul class="nav">
-      <li class="active">
-        <a href="{{ url('/home') }}">
-          <i class="ti-pie-chart"></i>
-          <p>Dashboard</p>
-        </a>
-      </li>
-      <li>
-          <a href="{{ url('/graphics/stars') }}">
-              <i class="ti-star"></i>
-              <p>Ranking Favoritados</p>
-          </a>
-      </li>
-      <li>
-          <a href="{{ url('/graphics/forks') }}">
-              <i class="ti-github"></i>
-              <p>Com mais Forks</p>
-          </a>
-      </li>
-    </ul>
-  </div>
 </div>
