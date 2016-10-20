d29d4899d9aa17d0cfea39b652eec0dea20ef17b
Gustavo Marttos <marttosc@gmail.com>
2016-10-20 21:09:13 -0200

    Update sidebar and assets using Blade.

--- src/app/Units/Core/Resources/Views/template/layout/app.blade.php
+++ src/app/Units/Core/Resources/Views/template/layout/app.blade.php
@@ -1,37 +1,37 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Stackhub</title>
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Scripts -->
     <script>
-      window.Laravel = <?php echo json_encode([
-        'csrfToken' => csrf_token(),
-      ]); ?>
+        window.Laravel = <?php echo json_encode([
+                'csrfToken' => csrf_token(),
+        ]); ?>
     </script>
-  </head>
-  <body>
+</head>
+<body>
 
-  <!-- Side -->
-  <div class="wrapper">
+<!-- Side -->
+<div class="wrapper">
     @if (Request::is('login'))
-      @include('core::template.snippets.navbar')
-      @yield('content')
-      @include('core::template.snippets.footer')
+        @include('core::template.snippets.navbar')
+        @yield('content')
+        @include('core::template.snippets.footer')
     @else
-    @include('core::template.snippets.sidebar')
+        @include('core::template.snippets.sidebar')
         <div class="main-panel">
-          @include('core::template.snippets.navbar')
-          @yield('content')
-          @include('core::template.snippets.footer')
+            @include('core::template.snippets.navbar')
+            @yield('content')
+            @include('core::template.snippets.footer')
         </div>
     @endif
-  </div>
-  <script src="/js/app.js"></script>
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
 
-  </body>
+</body>
 </html>
--- src/app/Units/Core/Resources/Views/template/snippets/sidebar.blade.php
+++ src/app/Units/Core/Resources/Views/template/snippets/sidebar.blade.php
@@ -8,19 +8,19 @@
             </a>
         </div>
         <ul class="nav">
-            <li>
+            <li {!! request()->route()->getName() == 'dashboard' ? 'class="active"' : '' !!}>
                 <a href="{{ url('/home') }}">
                     <i class="fa fa-dashboard"></i>
                     <p>Dashboard</p>
                 </a>
             </li>
-            <li>
+            <li {!! request()->route()->getName() == 'trends.stars' ? 'class="active"' : '' !!}>
                 <a href="{{ url('/graphics/stars') }}">
                     <i class="fa fa-star"></i>
                     <p>Top 10 Stared</p>
                 </a>
             </li>
-            <li>
+            <li {!! request()->route()->getName() == 'trends.forks' ? 'class="active"' : '' !!}>
                 <a href="{{ url('/graphics/forks') }}">
                     <i class="fa fa-code-fork"></i>
                     <p>Top 10 Forked</p>
