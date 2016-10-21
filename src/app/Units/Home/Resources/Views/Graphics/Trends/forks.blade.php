15e54db57ded5646a44926cd6c09c7d4e88e3464
Gustavo Marttos <marttosc@gmail.com>
2016-10-20 22:23:00 -0200

    First view of the most forked repos graphic.

--- src/app/Units/Home/Resources/Views/Graphics/Trends/forks.blade.php
+++ src/app/Units/Home/Resources/Views/Graphics/Trends/forks.blade.php
@@ -6,14 +6,43 @@
         <hr>
     </div>
     <div class="row">
-      <div id="forks"></div>
+        <div id="forks"></div>
     </div>
 @endsection
 
 @section('scripts')
     <script type="text/javascript">
-        $(function() {
-            Highcharts.chart('forks', {});
+        $(function () {
+            Highcharts.chart('forks', {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'Most forked repositories - Top 10'
+                },
+                xAxis: {
+                    categories: {!! $repositories->map(function($repo) { return $repo->repository . ' - ' . $repo->language; }) !!}
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Total de forks'
+                    }
+                },
+                plotOptions: {
+                    column: {
+                        stacking: 'normal',
+                        dataLabels: {
+                            enabled: true,
+                            color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+                        }
+                    }
+                },
+                series: [{
+                    name: 'Forks',
+                    data: {!! $repositories->map(function($repo) { return $repo->forks; }) !!}
+                }]
+            });
         });
     </script>
 @endsection
