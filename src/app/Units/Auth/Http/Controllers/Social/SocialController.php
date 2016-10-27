8011b8c84e98a973f3a712ae451c824dde358646
Gustavo Marttos <marttosc@gmail.com>
2016-10-27 17:51:05 -0200

    Fix conflicts, pt. 2

--- src/app/Units/Auth/Http/Controllers/Social/SocialController.php
+++ src/app/Units/Auth/Http/Controllers/Social/SocialController.php
@@ -5,12 +5,9 @@ namespace App\Units\Auth\Http\Controllers\Social;
 use Auth;
 use Socialite;
 use App\Domains\Users\User;
-<<<<<<< HEAD
-=======
 use App\Domains\Users\Mails\SendMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
->>>>>>> origin/master
 use Codecasts\Support\Http\Controller;
 
 abstract class SocialController extends Controller
@@ -18,11 +15,7 @@ abstract class SocialController extends Controller
     protected $provider;
 
     /**
-<<<<<<< HEAD
-     * Redirect to provider.
-=======
      * Redirect to social network auth provider.
->>>>>>> origin/master
      * @return mixed
      */
     public function redirectToProvider()
@@ -30,14 +23,9 @@ abstract class SocialController extends Controller
         return Socialite::driver($this->provider)->redirect();
     }
 
-<<<<<<< HEAD
-    /*
-     * Handle the provider callback.
-=======
     /**
      * Catch the user information from the social network.
      * @return \Illuminate\Http\RedirectResponse
->>>>>>> origin/master
      */
     public function handleProviderCallback()
     {
@@ -65,11 +53,7 @@ abstract class SocialController extends Controller
             return $authUser;
         }
 
-<<<<<<< HEAD
-        return User::create([
-=======
         $user = User::create([
->>>>>>> origin/master
             'provider'    => $this->provider,
             'name'        => $user->name,
             'email'       => $user->email,
@@ -77,12 +61,9 @@ abstract class SocialController extends Controller
             'avatar'      => $user->avatar,
             'extras'      => [],
         ]);
-<<<<<<< HEAD
-=======
 
         Mail::to($user)->send(new SendMail($user));
 
         return $user;
->>>>>>> origin/master
     }
 }
