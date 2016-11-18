8ba4840f4e00ff9665037f935821898b3beb2c30
Jordana <jordanasnogueira@gmail.com>
2016-11-18 14:58:38 -0200

    incluindo texto ao grafico de trending

--- app/Units/Home/Resources/Views/Graphics/learning_curve/languages.blade.php
+++ app/Units/Home/Resources/Views/Graphics/learning_curve/languages.blade.php
@@ -25,7 +25,7 @@
                 <h2><i class="ti-direction-alt"></i> Saiba Mais</h2>
             </div>
               <div class="content">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  Com base nos dados gerados pela curva de aprendizado, todos os pontos de <strong>todas as linguagens</strong> foram analisados, procurando um padrão de comportamento entre si. Utilizando a regressão linear a qual foi aplicada neste gráfico, você conseguirá identificar uma tendência perante essas linguagens de um determinado período.
               </div>
           </div>
       </div>
